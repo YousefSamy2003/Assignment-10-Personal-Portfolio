a06ca7254818056c0c51a584d04dceaf3d724884
YousefSamy2003 <yousafsamy9@outlook.com>
2026-01-02 23:05:55 +0200

    scrollSpy

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,24 @@
-// ^ Write your JavaScript code here
+//
+allSection = document.querySelectorAll("section");
+allLink = document.querySelectorAll("nav a");
 
+window.addEventListener("scroll", function () {
+  scrollSpy();
+});
 
-
-
+function scrollSpy() {
+  var storeSection;
+  for (let i = 0; i < allSection.length; i++) {
+    if (this.scrollY >= allSection[i].offsetTop) {
+      storeSection = allSection[i].getAttribute("id");
+    }
+  }
+  for (let i = 0; i < allLink.length; i++) {
+    if (allLink[i].getAttribute("href") == "#" + storeSection) {
+      allLink[i].classList.add("active");
+    } else {
+      allLink[i].classList.remove("active");
+    }
+  }
+}
+scrollSpy();
