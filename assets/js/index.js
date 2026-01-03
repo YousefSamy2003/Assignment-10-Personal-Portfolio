fc67f7ca44f720e04fa1fbf4901ec1acad968f04
YousefSamy2003 <yousafsamy9@outlook.com>
2026-01-03 18:44:37 +0200

    change font

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 //
-allSection = document.querySelectorAll("section");
-allLink = document.querySelectorAll("nav a");
+const allSection = document.querySelectorAll("section");
+const allLink = document.querySelectorAll("nav a");
 
 window.addEventListener("scroll", function () {
   scrollSpy();
@@ -24,8 +24,8 @@ function scrollSpy() {
 scrollSpy();
 
 //                 change Theme And Store in local storage
-btnMode = document.getElementById("theme-toggle-button");
-htmlPage = document.querySelector("html");
+const btnMode = document.getElementById("theme-toggle-button");
+const htmlPage = document.querySelector("html");
 
 function storeTheme() {
   if (localStorage.getItem("theme") == "light") {
@@ -48,8 +48,8 @@ storeTheme();
 
 /////                            open sideBar
 
-sideBarBtn = document.getElementById("settings-toggle");
-sideBar = document.getElementById("settings-sidebar");
+const sideBarBtn = document.getElementById("settings-toggle");
+const sideBar = document.getElementById("settings-sidebar");
 
 sideBarBtn.addEventListener("click", function () {
   sideBar.classList.toggle("translate-x-full");
@@ -58,7 +58,7 @@ sideBarBtn.addEventListener("click", function () {
 
 ////                    scroll to top button
 
-scrollToTopBtn = document.getElementById("scroll-to-top");
+const scrollToTopBtn = document.getElementById("scroll-to-top");
 window.addEventListener("scroll", function () {
   if (this.scrollY > allSection[0].offsetTop) {
     scrollToTopBtn.classList.remove("opacity-0");
@@ -71,3 +71,29 @@ window.addEventListener("scroll", function () {
 scrollToTopBtn.addEventListener("click", function () {
   location.href = "#hero-section";
 });
+
+///              handle     fonts
+
+const myAllFontsBtn = document.querySelectorAll(".my-fonts button");
+
+for (let i = 0; i < myAllFontsBtn.length; i++) {
+  myAllFontsBtn[i].addEventListener("click", function () {
+    for (let j = 0; j < myAllFontsBtn.length; j++) {
+      myAllFontsBtn[j].classList.remove("active");
+    }
+    myAllFontsBtn[i].classList.add("active");
+    const font = myAllFontsBtn[i].getAttribute("data-font");
+    document.body.classList.remove(
+      "font-cairo",
+      "font-alexandria",
+      "font-tajawal"
+    );
+    if (font === "alexandria") {
+      document.body.classList.add("font-alexandria");
+    } else if (font === "cairo") {
+      document.body.classList.add("font-cairo");
+    } else if (font === "tajawal") {
+      document.body.classList.add("font-tajawal");
+    }
+  });
+}
